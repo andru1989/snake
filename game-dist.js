6d5b256dd42ab1ee570a7e0ebc2ba2ebfa21fe6b
Andres Felipe Vizcaino Restrepo <andruvizcaino1989@gmail.com>
2016-07-14 13:56:30 -0500

    add food for snake and functionality to eat

--- game-dist.js
+++ game-dist.js
@@ -5,12 +5,54 @@ var _createClass = (function () { function defineProperties(target, props) { for
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
 ;(function () {
+  var Random = (function () {
+    function Random() {
+      _classCallCheck(this, Random);
+    }
+
+    _createClass(Random, null, [{
+      key: 'get',
+      value: function get(inicio, final) {
+        return Math.floor(Math.random() * final) + inicio;
+      }
+    }]);
+
+    return Random;
+  })();
+
+  var Food = (function () {
+    function Food(x, y) {
+      _classCallCheck(this, Food);
+
+      this.x = x;
+      this.y = y;
+      this.width = 10;
+      this.height = 10;
+    }
+
+    _createClass(Food, [{
+      key: 'draw',
+      value: function draw() {
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+      }
+    }], [{
+      key: 'generate',
+      value: function generate() {
+        return new Food(Random.get(0, 500), Random.get(0, 300));
+      }
+    }]);
+
+    return Food;
+  })();
+
   var Square = (function () {
     function Square(x, y) {
       _classCallCheck(this, Square);
 
       this.x = x;
       this.y = y;
+      this.width = 10;
+      this.height = 10;
       this.back = null // last square
       ;
     }
@@ -18,7 +60,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     _createClass(Square, [{
       key: 'draw',
       value: function draw() {
-        ctx.fillRect(this.x, this.y, 10, 10);
+        ctx.fillRect(this.x, this.y, this.width, this.height);
         if (this.hasBack()) {
           this.back.draw();
         }
@@ -67,6 +109,30 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
         this.copy();
         this.y += 10;
       }
+    }, {
+      key: 'hit',
+      value: function hit(head) {
+        var second = arguments.length <= 1 || arguments[1] === undefined ? false : arguments[1];
+
+        if (this == head && !this.hasBack()) return false;
+        if (this == head) return this.back.hit(head, true);
+
+        if (second && !this.hasBack()) return false;
+        if (second) return this.back.hit(head);
+
+        if (this.hasBack()) {
+          return squareHit(this, head) || this.back.hit(head);
+        }
+
+        return squareHit(this, head);
+      }
+    }, {
+      key: 'hitBorder',
+      value: function hitBorder() {
+        console.log('x: ' + this.x);
+        console.log('y: ' + this.y);
+        return this.x > 490 || this.x < 0 || this.y > 290 || this.y < 0;
+      }
     }]);
 
     return Square;
@@ -89,22 +155,28 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
       }
     }, {
       key: 'right',
+
+      // Avoid snake come back by the same way
       value: function right() {
+        if (this.direction === 'left') return;
         this.direction = 'right';
       }
     }, {
       key: 'left',
       value: function left() {
+        if (this.direction === 'right') return;
         this.direction = 'left';
       }
     }, {
       key: 'up',
       value: function up() {
+        if (this.direction === 'down') return;
         this.direction = 'up';
       }
     }, {
       key: 'down',
       value: function down() {
+        if (this.direction === 'up') return;
         this.direction = 'down';
       }
     }, {
@@ -115,6 +187,19 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
         if (this.direction === 'left') return this.head.left();
         if (this.direction === 'right') return this.head.right();
       }
+    }, {
+      key: 'eat',
+      value: function eat() {
+        //TODO: accumulate score
+        this.head.add();
+      }
+    }, {
+      key: 'dead',
+      value: function dead() {
+        // Chocar conmigo mismo
+        // Chocar con los bordes
+        return this.head.hit(this.head) || this.head.hitBorder();
+      }
     }]);
 
     return Snake;
@@ -122,11 +207,13 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
+  var score = 0;
 
   var snake = new Snake();
+  var foods = [];
 
   window.addEventListener('keydown', function (ev) {
-    ev.preventDefault();
+    if (ev.keyCode > 36 && ev.keyCode < 41) ev.preventDefault();
     if (ev.keyCode === 40) return snake.down();
     if (ev.keyCode === 39) return snake.right();
     if (ev.keyCode === 38) return snake.up();
@@ -135,11 +222,79 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     return false;
   });
 
-  setInterval(function () {
+  var animation = setInterval(function () {
     snake.move();
     // Borra el cuadrado de acuerdo a la coordenada x, y
     // n cantidad de pixeles hacia la derecha y hacia abajo
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     snake.draw();
+    drawFood();
+
+    if (snake.dead()) {
+      window.clearInterval(animation);
+    }
   }, 1000 / 10);
+
+  setInterval(function () {
+    var food = Food.generate();
+    foods.push(food);
+
+    setTimeout(function () {
+      // Elimina la comida
+      removeFromFoods(food);
+    }, 10000);
+  }, 4000);
+
+  function drawFood() {
+    for (var index in foods) {
+      var food = foods[index];
+
+      if (typeof food !== 'undefined') {
+        food.draw();
+        if (hit(food, snake.head)) {
+          snake.eat();
+          removeFromFoods(food);
+        }
+      }
+    }
+  }
+
+  function removeFromFoods(food) {
+    foods = foods.filter(function (f) {
+      return food !== f;
+    });
+  }
+
+  function squareHit(square_1, square_2) {
+    return square_1.x == square_2.x && square_1.y == square_2.y;
+  }
+
+  function hit(a, b) {
+    //Check if a collisions with b
+    var hit = false;
+
+    // Colisiones horizontales
+    if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+      // Colisiones verticales
+      if (b.y + b.height >= a.y && b.y < a.y + a.height) {
+        hit = true;
+      }
+    }
+
+    // Colisiones de a con b
+    if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+      if (b.y <= a.y && b.y + b.height >= a.y + a.height) {
+        hit = true;
+      }
+    }
+
+    // Colisiones de b con a
+    if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+      if (a.y <= b.y && a.y + a.height >= b.y + b.height) {
+        hit = true;
+      }
+    }
+
+    return hit;
+  }
 })();
--- game.js
+++ game.js
@@ -1,14 +1,38 @@
 ;(function(){
+  class Random{
+    static get(inicio, final){
+      return Math.floor(Math.random() * final) + inicio
+    }
+  }
+
+  class Food{
+    constructor(x,y){
+      this.x = x
+      this.y = y
+      this.width = 10
+      this.height = 10
+    }
+
+    draw(){
+      ctx.fillRect(this.x,this.y,this.width,this.height)
+    }
+
+    static generate(){
+      return new Food(Random.get(0,500), Random.get(0,300))
+    }
+  }
 
   class Square{
     constructor(x,y){
       this.x = x
       this.y = y
+      this.width = 10
+      this.height = 10
       this.back = null // last square
     }
 
     draw(){
-      ctx.fillRect(this.x,this.y,10,10)
+      ctx.fillRect(this.x,this.y,this.width,this.height)
       if(this.hasBack()){
         this.back.draw()
       }
@@ -47,6 +71,25 @@
       this.copy()
       this.y += 10
     }
+    hit(head, second = false){
+      if(this == head && !this.hasBack()) return false
+      if(this == head) return this.back.hit(head, true)
+
+      if(second && !this.hasBack()) return false
+      if(second) return this.back.hit(head)
+
+      if(this.hasBack()){
+        return squareHit(this,head) || this.back.hit(head)
+      }
+
+      return squareHit(this,head)
+    }
+
+    hitBorder(){
+      console.log(`x: ${this.x}`)
+      console.log(`y: ${this.y}`)
+      return this.x > 490 || this.x < 0 || this.y > 290 || this.y < 0
+    }
   }
 
   class Snake{
@@ -61,16 +104,21 @@
       this.head.draw()
     }
 
+    // Avoid snake come back by the same way
     right(){
+      if(this.direction === 'left') return
       this.direction = 'right'
     }
     left(){
+      if(this.direction === 'right') return
       this.direction = 'left'
     }
     up(){
+      if(this.direction === 'down') return
       this.direction = 'up'
     }
     down(){
+      if(this.direction === 'up') return
       this.direction = 'down'
     }
     move(){
@@ -79,15 +127,26 @@
       if(this.direction === 'left') return this.head.left()
       if(this.direction === 'right') return this.head.right()
     }
+    eat(){
+      //TODO: accumulate score
+      this.head.add()
+    }
+    dead(){
+      // Chocar conmigo mismo
+      // Chocar con los bordes
+      return this.head.hit(this.head) || this.head.hitBorder()
+    }
   }
 
   const canvas = document.getElementById('canvas')
   const ctx = canvas.getContext('2d')
+  const score = 0
 
   const snake = new Snake()
+  let foods = []
 
   window.addEventListener('keydown', function(ev){
-    ev.preventDefault()
+    if(ev.keyCode > 36 && ev.keyCode < 41) ev.preventDefault()
     if(ev.keyCode === 40) return snake.down();
     if(ev.keyCode === 39) return snake.right();
     if(ev.keyCode === 38) return snake.up();
@@ -96,12 +155,82 @@
     return false
   })
 
-  setInterval(function(){
+  const animation = setInterval(function(){
     snake.move()
     // Borra el cuadrado de acuerdo a la coordenada x, y
     // n cantidad de pixeles hacia la derecha y hacia abajo
     ctx.clearRect(0,0,canvas.width, canvas.height)
     snake.draw()
+    drawFood()
+
+    if(snake.dead()){
+      window.clearInterval(animation)
+    }
   }, 1000 / 10)
+
+  setInterval(function(){
+    const food = Food.generate()
+    foods.push(food)
+
+    setTimeout(function(){
+      // Elimina la comida
+      removeFromFoods(food)
+    }, 10000)
+
+  }, 4000)
+
+  function drawFood(){
+    for(const index in foods){
+      const food = foods[index]
+
+      if(typeof food !== 'undefined'){
+        food.draw()
+        if(hit(food,snake.head)){
+          snake.eat()
+          removeFromFoods(food)
+        }
+      }
+
+    }
+  }
+
+  function removeFromFoods(food){
+    foods = foods.filter(function(f){
+      return food !== f
+    })
+  }
+
+  function squareHit(square_1, square_2){
+    return square_1.x == square_2.x && square_1.y == square_2.y
+  }
+
+  function hit(a,b){
+    //Check if a collisions with b
+    var hit = false;
+
+    // Colisiones horizontales
+    if(b.x + b.width >= a.x && b.x < a.x + a.width){
+      // Colisiones verticales
+      if (b.y + b.height >= a.y && b.y < a.y + a.height){
+        hit = true
+      }
+    }
+
+    // Colisiones de a con b
+    if(b.x <= a.x && b.x + b.width >= a.x + a.width){
+      if(b.y <= a.y && b.y + b.height >= a.y +  a.height){
+        hit = true;
+      }
+    }
+
+    // Colisiones de b con a
+    if(a.x <= b.x && a.x + a.width >= b.x + b.width){
+      if(a.y <= b.y && a.y + a.height >= b.y + b.height){
+        hit = true;
+      }
+    }
+
+    return hit;
+  }
 
 })();
